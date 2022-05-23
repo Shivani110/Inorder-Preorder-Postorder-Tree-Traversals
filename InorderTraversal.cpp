bcb5ac1faa638400448964bdc9af2bedf0ac6ad1
Shivani Negi <78957674+Shivani110@users.noreply.github.com>
2022-05-23 20:09:46 +0530

    Add files via upload

---
+++ InorderTraversal.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>  
+using namespace std;  
+struct node {  
+    int n;  
+    struct node* l_side;  
+    struct node* r_side;  
+};    
+struct node* New(int x)  
+{  
+    struct node* m = (struct node*)malloc(sizeof(struct node));  
+    m->n = x;  
+    m->l_side = NULL;  
+    m->r_side = NULL;  
+    return (m);  
+}  
+
+void IN_traversal(struct node* Root_node)  
+{  
+    if (Root_node == NULL)  
+        return;  
+    IN_traversal(Root_node->l_side);  
+    cout<<" "<<Root_node->n<<" ";  
+    IN_traversal(Root_node->r_side);  
+}  
+int main()  
+{  
+    struct node* Root_node = New(41);  
+    Root_node->l_side = New(30);  
+    Root_node->r_side = New(56);  
+    Root_node->l_side->l_side = New(23);  
+    Root_node->l_side->r_side = New(35);    
+    Root_node->r_side->l_side = New(52);  
+    Root_node->r_side->r_side = New(60);  
+     
+    cout<<"\n The Inorder traversal of given binary tree is -\n";  
+    IN_traversal(Root_node);  
+    return 0;  
+}  
+
